e6e077b2a23f00cdf6a153ae2ea8bfa2eb9d054c
nikcbg <nikcbg@yahoo.com>
2021-01-14 10:11:33 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+variable "access_key" {}
+variable "secret_key" {}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+
 resource "aws_instance" "nikolay-test" {
   
 }
